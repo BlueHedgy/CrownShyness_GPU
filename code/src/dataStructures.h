15c500be455775931b2143f30892f001437f0711
NGUYEN Phu Hung <hung.a.nguyen@inria.fr>
2024-02-20 09:14:17 +0100

    migrate the current in-use data structures to a separate header file

---
+++ code/src/dataStructures.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream> 
+#include <math.h>      
+
+#include <LavaCake/Math/basics.h>
+#include <vector>
+
+using namespace LavaCake;
+
+struct Coord{
+    uint32_t gridIndex;     // the layer which the point belong to
+    vec2u coord;            // the cell of said layer
+    u_int16_t pointIndex;   // the point in the cell 
+};
+
+struct Edge{
+    Coord c1, c2;
+};
+
+struct points{
+    LavaCake::vec3f points;
+};
+
+struct Cell{
+    std::vector<LavaCake::vec2f> points;
+};
+
+struct Grid2D{
+    std::vector<std::vector<Cell>> cells;
+};
+
