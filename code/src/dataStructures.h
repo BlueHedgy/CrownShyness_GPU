4091227b79c371dc46bf141592829ae3df39bd1f
Hung Nguyen Phu <hung.a.nguyen@inria.fr>
2024-03-27 15:38:05 +0100

    add a weight field to coord structure to support the new inverted algorithm, which propagates the weight of the bottom layer thorugh out the tree on top

--- code/src/dataStructures.h
+++ code/src/dataStructures.h
@@ -13,7 +13,8 @@ using namespace LavaCake;
 struct Coord{
     uint32_t gridIndex;     // the layer which the point belong to
     vec2u coord;            // the cell of said layer
-    u_int16_t pointIndex;   // the index of the point in the cell 
+    u_int16_t pointIndex;   // the index of the point in the cell
+    float weight; 
 };
 
 struct Edge{
